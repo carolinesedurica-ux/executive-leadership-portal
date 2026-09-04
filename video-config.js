6c5b07bd6488f5d9348b35b889a0bc4a88c2dbcf
carolinesedurica-ux <carolines.edurica@gmail.com>
2026-09-04 21:05:45 +0200

    Wire Week 1-3 Vercel Blob coaching videos

--- video-config.js
+++ video-config.js
@@ -1,7 +1,6 @@
 // Vercel Blob video URLs for the Executive Leadership Readiness Programme.
-// Paste the final public Blob URLs here after upload.
 window.ELRP_VIDEO_URLS = {
-  week1: '',
-  week2: '',
-  week3: ''
+  week1: 'https://qxqravtajqj1esoa.public.blob.vercel-storage.com/Leadership_Identity%20%281%29.mp4',
+  week2: 'https://qxqravtajqj1esoa.public.blob.vercel-storage.com/Executive_Presence%20%281%29.mp4',
+  week3: 'https://qxqravtajqj1esoa.public.blob.vercel-storage.com/Speak_Clearly.mp4'
 };
