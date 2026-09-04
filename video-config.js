b47a8b847f5a4da12af4f19eb7f877fa4d0b6391
carolinesedurica-ux <carolines.edurica@gmail.com>
2026-09-04 20:51:27 +0200

    Add configurable hosted video URLs

---
+++ video-config.js
@@ -0,0 +1,7 @@
+// Vercel Blob video URLs for the Executive Leadership Readiness Programme.
+// Paste the final public Blob URLs here after upload.
+window.ELRP_VIDEO_URLS = {
+  week1: '',
+  week2: '',
+  week3: ''
+};
