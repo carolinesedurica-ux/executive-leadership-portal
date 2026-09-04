61cd7462e602634fef2a4717127c74ea52e82e88
carolinesedurica-ux <carolines.edurica@gmail.com>
2026-09-04 22:16:27 +0200

    Add portal logout endpoint

---
+++ api/logout.js
@@ -0,0 +1,7 @@
+const { clearSession, json } = require('./_lib');
+
+module.exports = async function handler(req, res) {
+  if (req.method !== 'POST') return json(res, 405, { error: 'Method not allowed' });
+  clearSession(res);
+  return json(res, 200, { ok: true });
+};
